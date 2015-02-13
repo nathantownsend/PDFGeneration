46e387a847eed8ed0761fd41ea96bc29be522b5b
Nathan Townsend <ntownsend2@mt.gov>
2015-02-13 14:24:10 -0700

    Initial code checkin
    The sample project demonstrates how to loop throuh sections, pages, and
    add instructions and rules. It uses a html template which is cool
    because our graphic designer can take the tamplate an add style.

---
+++ PDFGenerationExample/PDFGenerationExample/PDFGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.xml;
+using System.IO;
+using iTextSharp.tool.xml;
+using System.Text.RegularExpressions;
+
+namespace PDFGenerationExample
+{
+    public class PDFGenerator : PdfPageEventHelper 
+    {
+        string _templatepath;
+
+        public PDFGenerator(string templatePath)
+        {
+            _templatepath = templatePath;
+
+        }
+
+        public void CreatePDF(string html, string outputPath)
+        {
+            WritePDF(html, outputPath);
+        }
+
+        public string MergeTemplate(string GeneratedContent)
+        {
+            string template = File.ReadAllText(_templatepath);
+            return template.Replace("#ContentBody#", GeneratedContent);
+        }
+
+
+        private void WritePDF(string html, string outputPath)
+        {
+            using (var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                using (var doc = new Document())
+                {
+                    using (var writer = PdfWriter.GetInstance(doc, fs))
+                    {
+                        //Open our document for writing
+                        doc.Open();
+
+                        //Fully qualified path to our font
+                        var verdana = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "Verdana.TTF");
+                        var times = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "Times.ttf ");
+
+                        //Register our font and give it an alias to reference in CSS
+                        var fontProv = new XMLWorkerFontProvider();
+                        fontProv.Register(verdana, "Verdana");
+                        fontProv.Register(times, "Times New Roman");
+
+                        //Extract the CSS from the html document
+                        Regex re = new Regex("<style type=\"text/css\">[^</style>]+</style>", RegexOptions.IgnoreCase| RegexOptions.Multiline);
+                        string css = re.Match(html).Value.Replace("<style type=\"text/css\">", "").Replace("</style>", "");
+                        
+
+                        //Create a stream to read our HTML
+                        using (var htmlMS = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(html)))
+                        {
+                            //Create a stream to read our CSS
+                            using (var cssMS = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(css)))
+                            {
+                                //Get an instance of the generic XMLWorker
+                                var xmlWorker = XMLWorkerHelper.GetInstance();
+
+                                //Parse our HTML using everything setup above
+                                xmlWorker.ParseXHtml(writer, doc, htmlMS, cssMS, System.Text.Encoding.UTF8, fontProv);
+                            }
+                        }
+
+                        //Close and cleanup
+                        doc.Close();
+
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
+
---
+++ PDFGenerationExample/PDFGenerationExample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFGenerationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string templatePath  = @"C:\Users\cb0290\Documents\GitHub\PDFGeneration\PDFGenerationExample\PDFGenerationExample\Template.html";
+
+            PDFGenerator generator = new PDFGenerator(templatePath);
+
+            string content = GenerateContentFromDatabase();
+
+            string html = generator.MergeTemplate(content);
+            
+            generator.CreatePDF(html, @"C:\temp\test.pdf");
+
+        }
+
+
+        private static string GenerateContentFromDatabase()
+        {
+            string[] sections = new string[] { "First", "Second", "Third" };
+            string[] pages = new string[] { "Page 1", "Page 2", "Page 3" };
+            Dictionary<string, string> rules = new Dictionary<string, string>();
+            rules.Add("303(1)(a)", "the name, permanent and temporary post office addresses, telephone numbers, and, as applicable, social security numbers and employer identification numbers of the applicant, applicant's resident agent, and the person who will pay the abandoned mine reclamation fee pursuant to 30 USC 1232");
+            rules.Add("303(1)(b)", "the names and addresses of any persons who are engaged in strip or underground mining on behalf of the applicant and any person who will conduct such operations should the permit be granted;");
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string section in sections)
+            {
+                sb.AppendFormat("<h2>{0}</h2>", section);
+                foreach (string page in pages)
+                {
+                    sb.AppendFormat("<h3>{0}</h3>", page);
+
+                    sb.Append("<h4>Instructions</h4>");
+                    sb.Append("<div class=\"instructions\">");
+                    sb.Append("Instructions go here"); // instructios from the help screen
+                    sb.Append("</div>");
+
+                    sb.Append("<h4>Rules</h4>");
+                    sb.Append("<div class=\"subsections\">");
+                    foreach (KeyValuePair<string, string> rule in rules)
+                    {
+                        string subsection = rule.Key;
+                        string description = rule.Value;
+                        sb.AppendFormat("<p><strong>{0}:</strong>{1}</p>", subsection, description); // append rules like this
+                    }
+                    sb.Append("</div>");
+                }
+
+            }
+
+            return sb.ToString();
+
+        }
+
+    }
+}
